15c751a34e77d186fe2f79a5ef1fd7b94bf2a600
Gerhard <gerhard.putter@yahoo.com>
2021-08-25 05:25:29 +0200

    Added Add and delete functionality to list.

--- js/list.js
+++ js/list.js
@@ -1,14 +1,18 @@
-let myList = ["bread", "lemons", "sugar", "cookies", "spices"]
+let myList = []
 
 const addBtn = document.querySelector("#addNew");
 const output = document.querySelector(".output");
+const newItem = document.querySelector("#addItem");
 
 addBtn.addEventListener('click', function () {
-    console.log("clicked")
+    if (newItem.value)
+        myList.push(newItem.value)
+    build()
 })
 window.onload = build
 
 function build() {
+    output.innerHTML = ""
     const table = document.createElement("table")
     for (let i = 0; i < myList.length; i++) {
         const row = document.createElement("tr");
@@ -19,6 +23,10 @@ function build() {
         const cell2 = document.createElement("td");
         const span1 = document.createElement("span")
         span1.innerText = "Delete"
+        span1.addEventListener("click", function () {
+            let itemOut = myList.splice(i, 1)
+            build()
+        })
         cell2.appendChild(span1)
         const span2 = document.createElement("span")
         span2.innerText = "Edit"
