7e868b8963bd849507339da06ddfa584fc146c13
Gerhard <gerhard.putter@yahoo.com>
2021-08-25 05:00:31 +0200

    Built list structure with Js.

--- js/list.js
+++ js/list.js
@@ -1,6 +1,8 @@
 let myList = ["bread", "lemons", "sugar", "cookies", "spices"]
 
 const addBtn = document.querySelector("#addNew");
+const output = document.querySelector(".output");
+
 addBtn.addEventListener('click', function () {
     console.log("clicked")
 })
@@ -15,8 +17,15 @@ function build() {
         cell1.innerHTML = myList[i]
         row.appendChild(cell1)
         const cell2 = document.createElement("td");
-
+        const span1 = document.createElement("span")
+        span1.innerText = "Delete"
+        cell2.appendChild(span1)
+        const span2 = document.createElement("span")
+        span2.innerText = "Edit"
+        cell2.appendChild(span2)
+        row.appendChild(cell2)
         table.appendChild(row)
     }
     console.log(table)
+    output.appendChild(table)
 }
